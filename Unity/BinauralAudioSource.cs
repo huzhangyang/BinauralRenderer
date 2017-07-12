a2a05e85c39165ce61a2c5fe250cd3050c36da88
huzhangyang <huzhangyang@gmail.com>
2017-07-12 20:42:38 +0100

    Add UnityScript.

---
+++ Unity/BinauralAudioSource.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*Binaural Audio Source. Use this on each audio source you want to perform binaural effect with.*/
+public class BinauralAudioSource : MonoBehaviour 
+{
+	/*Path of the audio file.*/
+	public string audioPath = @"C:\test2.mp3";
+	/*An unique id used to identify the audio source object.*/
+	public string sourceID = "1";
+	/*Specify the hrtf effect.*/
+	public bool hrtf = true;
+
+	void Start () 
+	{
+		BinauralEngine.AddAudioSource (audioPath, sourceID);
+		BinauralEngine.SetAudioSourcePos (sourceID, transform.position.x, transform.position.y, transform.position.z);
+		BinauralEngine.PlayAudioSource (sourceID);
+	}
+		
+	void Update () 
+	{
+		BinauralEngine.SetAudioSourceHRTF (sourceID, hrtf);
+		BinauralEngine.SetAudioSourcePos (sourceID, transform.position.x, transform.position.y, transform.position.z);
+	}
+
+	void OnEnable()
+	{
+		BinauralEngine.SetAudioSourcePaused (sourceID, false);
+	}
+
+	void OnDisable()
+	{
+		BinauralEngine.SetAudioSourcePaused (sourceID, true);
+	}
+
+	void OnDestroy()
+	{
+		BinauralEngine.StopAudioSource (sourceID);
+		BinauralEngine.RemoveAudioSource (sourceID);
+	}
+
+	void OnApplicationPause(bool paused)
+	{
+		BinauralEngine.SetAudioSourcePaused (sourceID, paused);
+	}
+}
---
+++ Unity/BinauralEngine.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.InteropServices;  
+
+/*Main Audio Engine. Use this on audio listener uniquely.*/
+public class BinauralEngine : MonoBehaviour 
+{
+	/*Must specify an matlab bin library to use*/
+	public string matlabPath = @"C:\Program Files\MATLAB\R2016b\bin\win64";
+
+	/*Must specify a CIPIC hrir file to use*/
+	public string hrtfPath = @"C:\test.mat";
+
+	/*public functions*/
+	[DllImport("BinauralRender")]  
+	public static extern void AddAudioSource (string filename, string sourceID);
+	[DllImport("BinauralRender")]  
+	public static extern void RemoveAudioSource (string sourceID);
+	[DllImport("BinauralRender")]  
+	public static extern void PlayAudioSource(string sourceID);
+	[DllImport("BinauralRender")]  
+	public static extern void StopAudioSource(string sourceID);
+	[DllImport("BinauralRender")]  
+	public static extern void SetAudioSourcePaused(string sourceID, bool paused);
+	[DllImport("BinauralRender")]  
+	public static extern bool IsAudioSourcePlaying(string sourceID);
+	[DllImport("BinauralRender")]  
+	public static extern void SetAudioSourceHRTF(string sourceID, bool enable);
+	[DllImport("BinauralRender")]  
+	public static extern void SetAudioSourcePos(string sourceID, float posx, float posy, float posz);
+
+	/*private functions*/
+	[DllImport("BinauralRender")]  
+	private static extern void UpdateAudioEngine ();
+	[DllImport("BinauralRender")]  
+	private static extern void ReleaseAudioEngine ();
+	[DllImport("BinauralRender")]  
+	private static extern void SetHRIR(string filename);
+	[DllImport("BinauralRender")]  
+	private static extern void SetListener(float posx, float posy, float posz, float orix, float oriy, float oriz);
+
+	void Awake()
+	{		
+		String currentPath = Environment.GetEnvironmentVariable("PATH");
+		if(currentPath.Contains(matlabPath) == false)
+		{
+			Environment.SetEnvironmentVariable("PATH", Environment.GetEnvironmentVariable("PATH") + ";" + matlabPath);
+		}
+	}
+
+	void Start () 
+	{
+        SetHRIR(hrtfPath);
+    }
+		
+	void Update () 
+	{
+		SetListener(transform.position.x, transform.position.y, transform.position.z,
+			transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+		UpdateAudioEngine();
+	}
+
+	void OnDestroy()
+	{
+		ReleaseAudioEngine ();
+	}
+
+	void OnApplicationQuit()
+	{
+		ReleaseAudioEngine ();
+	}
+}
